4b91371a660321b13c7e8de5cf9fa7fd869f170c
Adarsh Maurya <adarshmaurya1000@gmail.com>
2021-01-19 12:02:02 +0530

    react front end test

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyD2tsin-cdY9FUXO6YPPKkeUDL-shCNReo",
+  authDomain: "confusionserver-e1bd7.firebaseapp.com",
+  projectId: "confusionserver-e1bd7",
+  storageBucket: "confusionserver-e1bd7.appspot.com",
+  messagingSenderId: "969751278717",
+  appId: "1:969751278717:web:1727361fde93c7eaac6d14",
+  measurementId: "G-NW8DC3HH6X"
+};
